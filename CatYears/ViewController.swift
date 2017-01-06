07549414bce3d09ae17308a4a2bda90fde417da6
Alexis Araujo <ala1289@gmail.com>
2017-01-05 20:30:30 -0600

    Adding code to calculate age cat

--- CatYears/ViewController.swift
+++ CatYears/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var ageLabel: UILabel!
+    
 
+    @IBAction func submitPressed(_ sender: Any) {
+        let age = Int(ageTextField.text!)! * 7
+        ageLabel.text = String(age)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +28,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
     }
 
 
